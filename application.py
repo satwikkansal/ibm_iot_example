e10bb27a9346ed69567110f7eec85fa76731280b
Satwik <satwikkansal@gmail.com>
2020-03-08 01:25:22 +0530

    Add commands logic and do some refactoring

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 import wiotp.sdk.application
 
-config = wiotp.sdk.application.parseConfigFile("app_config.yml")
-app_client = wiotp.sdk.application.ApplicationClient(config)
 
-cloudant_creds = {
+CLOUDANT_CREDS = {
   "apikey": "your_api_key_here",
   "host": "eeca8e52-1774-4698-93d1-cafa434762ac-bluemix.cloudantnosqldb.appdomain.cloud",
   "password": "your_pwd_here",
@@ -11,36 +9,52 @@ cloudant_creds = {
   "username": "your_username_here"
 }
 
-service_binding = {
+SERVICE_BINDING = {
     "name": "any-binding-name",
     "description": "Test Cloudant Binding",
     "type": "cloudant",
-    "credentials": cloudant_creds
+    "credentials": CLOUDANT_CREDS
 }
 
-# Bind application to the cloudant DB
-cloudant_service = app_client.serviceBindings.create(service_binding)
+ANDROID_DEVICE_TYPE = "Android"
+GATEWAY_DEVICE_TYPE = "raspi"
+STATUS_EVENT_TYPE = "status"
 
-# Create the connector
-connector = app_client.dsc.create(
-    name="connector_1", type="cloudant", serviceId=cloudant_service.id, timezone="UTC",
-    description="Data connector", enabled=True
-)
 
-# Create a destination under the connector
-destination_1 = connector.destinations.create(name="sensor-data", bucketInterval="DAY")
+def get_application_client(config_file_path):
+    config = wiotp.sdk.application.parseConfigFile(config_file_path)
+    app_client = wiotp.sdk.application.ApplicationClient(config)
+    return app_client
 
-# Create a rule under the connector, that routes all Android status events to the destination
-rule_1 = connector.rules.createEventRule(
-    name="status_events", destinationName=destination_1.name, typeId="Android", eventId="status",
-    description="Send android status events", enabled=True
-)
 
-# Create another destination under the connector
-destination_2 = connector.destinations.create(name="gateway-data", bucketInterval="DAY")
+def create_cloudant_connections(client, service_binding):
+    # Bind application to the Cloudant DB
+    cloudant_service = client.serviceBindings.create(service_binding)
 
-# Create a rule under the connector, that routes all raspi status events to the destination
-rule_2 = connector.rules.createEventRule(
-    name="status_events", destinationName=destination_2.name, typeId="raspi", eventId="status",
-    description="Gateway status events", enabled=True
-)
+    # Create the connector
+    connector = client.dsc.create(
+        name="connector_1", type="cloudant", serviceId=cloudant_service.id, timezone="UTC",
+        description="Data connector", enabled=True
+    )
+
+    # Create a destination under the connector
+    destination_1 = connector.destinations.create(name="sensor-data", bucketInterval="DAY")
+
+    # Create a rule under the connector, that routes all Android status events to the destination
+    connector.rules.createEventRule(
+        name="status_events", destinationName=destination_1.name, typeId=ANDROID_DEVICE_TYPE, eventId=STATUS_EVENT_TYPE,
+        description="Send android status events", enabled=True
+    )
+
+    # Create another destination under the connector
+    destination_2 = connector.destinations.create(name="gateway-data", bucketInterval="DAY")
+
+    # Create a rule under the connector, that routes all raspi status events to the destination
+    connector.rules.createEventRule(
+        name="status_events", destinationName=destination_2.name, typeId=GATEWAY_DEVICE_TYPE, eventId=STATUS_EVENT_TYPE,
+        description="Gateway status events", enabled=True)
+
+
+def send_reset_command(client, type, id):
+    data = {'reset': True}
+    client.publishCommand(type, id, "reset", "json", data)
--- main.py
+++ main.py
@@ -26,7 +26,28 @@ def send_device_readings():
         send_android_device_event(client, device_addr, "status", data)
     devices_data.clear()
 
+
+def gateway_command_callback(cmd):
+    print("Command received for {}:{}: {}".format(cmd.typeId, cmd.deviceId, cmd.data))
+    if cmd.typeId == 'reset':
+        reset_data(devices_data)
+    else:
+        print("Unknown command type received")
+
+
+def reset_data():
+    devices_data.clear()
+    # Add more custom logic here.
+    pass
+
+
 tl.start()
+
+# Subscribing to commands
+client.subscribeToCommands(commandId="reset")
+# Registering a callback
+client.commandCallback = gateway_command_callback
+
 
 for data, device_addr in listen_sensor_data():
     devices_data[device_addr[0]] = data
